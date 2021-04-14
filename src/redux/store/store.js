936b49d9391de59393758416b2ebb4a4f272ed9f
MikhailR1337 <mihail.rogov@mail.ru>
2021-04-14 12:38:04 +0300

    add middleware - logger

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from '../reducers/rootReducer';
+import { loggerMiddleware } from '../middlewares/middlewares';
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(loggerMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
